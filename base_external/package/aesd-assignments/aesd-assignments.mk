e313b64dd0eaf112c5606239c0ed4e07614ff5f2
PlaceholderForAProperUsername <thorsten.hoffmeister@protonmail.com>
2023-07-18 12:25:16 +0200

    test

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '83a59d7579000c3838fdfbdc66b7a44400ecb217'
+AESD_ASSIGNMENTS_VERSION = '8ffc00771af034a4a18516464d57798cca02e545'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
