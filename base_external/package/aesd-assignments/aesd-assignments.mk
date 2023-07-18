ace4acf4bc8cae61b38c208cf9ecb31c8b04b7bb
PlaceholderForAProperUsername <thorsten.hoffmeister@protonmail.com>
2023-07-18 12:17:44 +0200

    test

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '63e745848c5c4b99b576cc53878dfe150b522fcd'
+AESD_ASSIGNMENTS_VERSION = '83a59d7579000c3838fdfbdc66b7a44400ecb217'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,8 +15,8 @@ AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+	# $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	# $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
